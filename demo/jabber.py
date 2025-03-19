1eda5d4efe73ebced2c2e6f2e6608155960fbff0
zach-karlovich <ddf4me@virginia.edu>
2025-03-18 20:05:35 -0400

    push challenge

---
+++ demo/jabber.py
@@ -0,0 +1,3 @@
+def tell_me_something():
+    """Wise cracking function"""
+    print("Something")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="demo",
+    version="0.1",
+    description="A simple demo package",
+    author="JZK",
+    author_email="jzk@example.com",
+    license="MIT",
+    packages=["demo"]
+)
